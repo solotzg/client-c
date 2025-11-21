0514696b5ab90ad3d5422067fcb446ca3fb92110
Shenghui Wu <wshwsh12@outlook.com>
2025-11-21 17:03:07 +0800

    tici: merge master into feature branch fts (#219)
    Signed-off-by: guo-shaoge <shaoge1994@163.com>
    Signed-off-by: Liqi Geng <gengliqiii@gmail.com>
    Signed-off-by: JaySon <tshent@qq.com>
    
    Co-authored-by: guo-shaoge <shaoge1994@163.com>
    Co-authored-by: Liqi Geng <gengliqiii@gmail.com>
    Co-authored-by: JaySon <tshent@qq.com>
    (cherry picked from commit 9edfcab93ee7ce084476d63ae127dbc5fc1a63fb)

--- include/pingcap/pd/Client.h
+++ include/pingcap/pd/Client.h
@@ -17,10 +17,9 @@
 #include <shared_mutex>
 #include <thread>
 
-namespace pingcap
+namespace pingcap::pd
 {
-namespace pd
-{
+
 class Client : public IClient
 {
     const int max_init_cluster_retries;
@@ -60,6 +59,10 @@ public:
     uint64_t getGCSafePoint() override;
 
     uint64_t getGCSafePointV2(KeyspaceID keyspace_id) override;
+
+    pdpb::GetGCStateResponse getGCState(KeyspaceID keyspace_id) override;
+
+    pdpb::GetAllKeyspacesGCStatesResponse getAllKeyspacesGCStates() override;
 
     KeyspaceID getKeyspaceID(const std::string & keyspace_name) override;
 
@@ -153,5 +156,4 @@ private:
 };
 
 
-} // namespace pd
-} // namespace pingcap
+} // namespace pingcap::pd
